b9d11b8e68dfe1bca608818cd81a525613d4557a
Ruthvik Mukkamala <ruthvikmukkamala@gmail.com>
2025-05-27 17:20:27 -0400

    add Deck header

---
+++ include/core/Deck.h
@@ -0,0 +1,25 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include "Card.h"
+#include <vector>
+#include <random>
+
+class Deck {
+private:
+	std::vector<Card> cards; 
+
+public:
+	Deck(); 
+
+	void reset(); 
+	void shuffle(); 
+
+	Card deal(); 
+	std::vector<Card> dealHand(int numCards); 
+
+	size_t size() const; 
+	bool isEmpty() const; 
+}
+
+#endif
